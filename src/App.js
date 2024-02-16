6acf83ad625973ec72096272c9b2f0a8cf770a11
Ashok Kumar <112795808+ashoka4223@users.noreply.github.com>
2024-02-16 15:36:53 +0530

    UI for job listing application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Create, Dashboard, Feed, Home } from "./pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employer" >
+          <Route path="/employer/dashboard" element={<Dashboard />}/>
+          <Route path="/employer/create" element={<Create />}/>
+          </Route>
+          <Route path="/employee/feed" element={<Feed />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Create } from './Create'
+export { default as Dashboard } from './Dashboard'
+export { default as Feed } from './Feed'
+export { default as Home } from './Home'
